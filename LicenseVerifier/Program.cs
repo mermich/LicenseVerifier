e4f1cf5efdb79ae1fa8fc97115e3680e058b8056
Mermillod Etienne <etienne_mermillod@hotmail.com>
2025-06-06 10:49:34 -0300

    avec fonction de chiffrage et numero de version

--- LicenseVerifier/Program.cs
+++ LicenseVerifier/Program.cs
@@ -1,3 +1,6 @@
+using System.Security.Cryptography;
+using System.Text;
+
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -8,28 +11,96 @@ WebApplication app = builder.Build();
 
 app.UseHttpsRedirection();
 
-string[] clesValides = new[]
-{
-    "toto", "toto1", "toto2"
-};
-
 
 app.MapGet("/verify/{cle}", (string cle) =>
 {
-    if (clesValides.Contains(decryptKey(cle)))
+    License? license = decryptKey(cle);
+    if (license == null)
+    {
+        return Results.BadRequest("Clé invalide ou mal formée");
+    }
+    else if (license.IsValid())
     {
         return Results.Ok("Clé valide");
     }
-    return Results.BadRequest("Clé invalide");
+    else
+    {
+        return Results.BadRequest("Clé invalide");
+    }
 });
 
 
 
-string decryptKey(string encryptedKey)
+License? decryptKey(string rawData)
 {
-    // Simulate decryption logic here
-    // In a real application, you would implement actual decryption logic
-    return encryptedKey; // For simplicity, returning the same key
+    string decryptedData = "";
+    using (SHA256 sha256Hash = SHA256.Create())
+    {
+        byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(rawData));
+        StringBuilder builder = new();
+        foreach (byte b in bytes)
+        {
+            _ = builder.Append(b.ToString("x2"));
+        }
+        decryptedData = builder.ToString();
+    }
+
+    return System.Text.Json.JsonSerializer.Deserialize<License>(decryptedData);
 }
 
+
 app.Run();
+
+public class License
+{
+    public int Version { get; set; }
+    public required string Key { get; set; }
+
+    private static readonly string[] clesValides = new[]
+    {
+        "toto", "toto1", "toto2"
+    };
+
+    public bool IsValid()
+    {
+        if (Version == 1)
+        {
+            if (clesValides.Contains(Key))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else if (Version == 2)
+        {
+            /// un truc plus complexe...
+            return false;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
+
+
+public class CoteClient
+{
+    // fonction de chiffrage SHA256
+    static string ComputeSha256Hash(string rawData)
+    {
+        using (SHA256 sha256Hash = SHA256.Create())
+        {
+            byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(rawData));
+            StringBuilder builder = new StringBuilder();
+            foreach (byte b in bytes)
+            {
+                builder.Append(b.ToString("x2"));
+            }
+            return builder.ToString();
+        }
+    }
+}
